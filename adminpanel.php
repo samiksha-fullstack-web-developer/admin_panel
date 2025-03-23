52b96e5f99dc8ba59dafee4e64b9d9d46ae22371
Samiksha <ssamiksha955@gmail.com>
2025-03-23 18:20:14 +0530

    Update adminpanel.php

--- adminpanel.php
+++ adminpanel.php
@@ -8,7 +8,6 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
-
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
 
