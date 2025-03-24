87b776b8a27eed76b139adc33183756d65370931
Samiksha <ssamiksha955@gmail.com>
2025-03-24 10:34:50 +0530

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -13,22 +13,12 @@ if ($conn->connect_error) {
 $dbname = "admin_portal";
 $sql = "CREATE DATABASE $dbname";
 
-if (mysqli_query($conn, $sql)) {
-  echo "Database created successfully.<br>";
-} else {
-  echo "Error creating database: " . mysqli_error($conn);
-}
-
 //connect to database
 mysqli_select_db($conn, $dbname);
 
 //create table
 $table = "CREATE TABLE admins(id int UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(30) NOT NULL, password VARCHAR(255) NOT NULL)";
 
-if (mysqli_query($conn, $table)) {
-  echo "Table Created Successfully.<br>";
-} else {
-  echo "Error Creating table: " . mysqli_error($conn);
-}
+mysqli_query($conn, $table)
 
 ?>
