c33b0328e8f33ed2df53af5bf9cc747e89ff4839
Samiksha <ssamiksha955@gmail.com>
2025-03-24 10:31:01 +0530

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -18,11 +18,11 @@ if (mysqli_query($conn, $sql)) {
 } else {
   echo "Error creating database: " . mysqli_error($conn);
 }
+
 //connect to database
 mysqli_select_db($conn, $dbname);
 
 //create table
-
 $table = "CREATE TABLE admins(id int UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(30) NOT NULL, password VARCHAR(255) NOT NULL)";
 
 if (mysqli_query($conn, $table)) {
